7320b21258b76427159c8183c72380c1cbccefc6
kapu <pjoonwoo111@gmail.com>
2026-03-29 16:11:35 +0900

    feat(reply): add ReplyTransitionEvent sealed interface

---
+++ app/src/main/java/party/qwer/iris/reply/ReplyTransitionEvent.kt
@@ -0,0 +1,9 @@
+package party.qwer.iris.reply
+
+sealed interface ReplyTransitionEvent {
+    data object PrepareStarted : ReplyTransitionEvent
+    data object PrepareCompleted : ReplyTransitionEvent
+    data object SendStarted : ReplyTransitionEvent
+    data object SendCompleted : ReplyTransitionEvent
+    data class Failed(val reason: String) : ReplyTransitionEvent
+}
