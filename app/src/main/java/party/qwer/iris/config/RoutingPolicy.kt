96c6a52603c7b2644d6be236f426f6470a17b1eb
kapu <pjoonwoo111@gmail.com>
2026-03-29 16:53:01 +0900

    feat(config): add RoutingPolicy data class

---
+++ app/src/main/java/party/qwer/iris/config/RoutingPolicy.kt
@@ -0,0 +1,7 @@
+package party.qwer.iris.config
+
+data class RoutingPolicy(
+    val commandRoutePrefixes: Map<String, List<String>>,
+    val imageMessageTypeRoutes: Map<String, List<String>>,
+    val requiresExternalBootstrap: Boolean,
+)
