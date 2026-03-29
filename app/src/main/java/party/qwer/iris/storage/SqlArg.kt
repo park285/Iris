d80ffc243cac73d7176e185748a7bad745154aab
kapu <pjoonwoo111@gmail.com>
2026-03-29 15:25:03 +0900

    feat(storage): add SqlArg sealed interface

---
+++ app/src/main/java/party/qwer/iris/storage/SqlArg.kt
@@ -0,0 +1,18 @@
+package party.qwer.iris.storage
+
+sealed interface SqlArg {
+    data class Str(val value: String) : SqlArg
+    data class LongVal(val value: Long) : SqlArg
+    data class IntVal(val value: Int) : SqlArg
+    data object Null : SqlArg
+}
+
+fun List<SqlArg>.toBindArray(): Array<String?> =
+    map { arg ->
+        when (arg) {
+            is SqlArg.Str -> arg.value
+            is SqlArg.LongVal -> arg.value.toString()
+            is SqlArg.IntVal -> arg.value.toString()
+            SqlArg.Null -> null
+        }
+    }.toTypedArray()
